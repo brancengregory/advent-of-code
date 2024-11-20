460546ee261ea93c860fee6ba21b94437177a231
Brancen Gregory <brancengregory@gmail.com>
2024-11-19 18:22:48 -0600

    2015 day 17 part 2

--- 2015/17/rust/src/main.rs
+++ 2015/17/rust/src/main.rs
@@ -1,36 +1,53 @@
 use std::{
     fs::read_to_string,
-    io::Result
+    io::Result,
 };
 use itertools::Itertools;
 
-fn read_input(path: &str) -> Result<Vec<String>> {
+fn read_input(path: &str) -> Result<Vec<i32>> {
     let content = read_to_string(path)?;
-
-    Ok(content.lines().map(|s| s.to_string()).collect())
+    Ok(
+        content
+            .lines()
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect::<Vec<_>>()
+    )
 }
 
 fn main() -> Result<()> {
-    let input: Vec<String> = read_input("../input")?;
+    let input: Vec<i32> = read_input("../input")?;
 
-    let powerset: Vec<_> = input.iter().powerset().collect();
-    
-    let matches = powerset
-        .into_iter()
-        .filter_map(|v| {
-            let v_num: Option<Vec<i32>> = v
-                .iter()
-                .map(|s| s.parse::<i32>().ok())
-                .collect();
+    // Generate the powerset of container indices to preserve uniqueness
+    let powerset = (0..input.len()).powerset();
 
-            v_num.map(|v| if v.iter().sum::<i32>() == 150 { v } else { vec![] })
+    let matches: Vec<Vec<usize>> = powerset
+        .filter_map(|indices| {
+            let sum: i32 = indices.iter().map(|&i| input[i]).sum();
+            if sum == 150 {
+                Some(indices)
+            } else {
+                None
+            }
         })
-        .filter(|v| !v.is_empty())
-        .collect::<Vec<_>>();
+        .collect();
 
-    let ans: usize = matches.len();
+    let ans = matches.len();
     
     println!("Part 1: {:?}", ans);
+
+    let min_len = matches
+        .iter()
+        .map(|v| v.len())
+        .min()
+        .unwrap();
     
+    let ans = matches
+        .iter()
+        .filter(|v| v.len() == min_len)
+        .count();
+    
+    println!("Part 2: {:?}", ans);
+
     Ok(())
 }
+
