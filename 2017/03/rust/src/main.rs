d53b7e526c708fd8de6269da2f82f5b48bd954de
Brancen Gregory <brancengregory@gmail.com>
2025-11-03 01:33:53 -0800

    Complete 2017 day 3 part 1

--- 2017/03/rust/src/main.rs
+++ 2017/03/rust/src/main.rs
@@ -13,12 +13,18 @@ fn main() {
 
 	println!("ring: {}, odd: {}, number: {}", ring, odd, number);
 
-	let coords: (i32, i32) = (ring, ring);
-	let side_length = odd - 1;
+	let mut coords: (i32, i32) = (ring, ring);
+	let side_length = ring * 2;
 	println!("coords: {:?}", coords);
 	println!("distance: {:?}", coords.0.abs() + coords.1.abs());
 
 	let diff = number - val;
 	println!("diff: {:?}", diff);
+
+	if !(diff < side_length) { panic!("diff ({}) not less than side length ({})", diff, side_length) };
+
+	coords.1 -= diff;
+	println!("coords: {:?}", coords);
+	println!("distance: {:?}", coords.0.abs() + coords.1.abs());
 }
 
