82196626ca8b136e415148fbad8671f073dfec81
Brancen Gregory <brancengregory@gmail.com>
2025-11-05 00:16:07 -0800

    Complete 2017 day 3

--- 2017/03/rust/src/main.rs
+++ 2017/03/rust/src/main.rs
@@ -1,3 +1,46 @@
+use std::collections::HashMap;
+
+enum Dir {
+	Left,
+	Right,
+	Up,
+	Down
+}
+
+fn turn(d: &Dir) -> Dir {
+	match d {
+		Dir::Right => Dir::Up,
+		Dir::Up => Dir::Left,
+		Dir::Left => Dir::Down,
+		Dir::Down => Dir::Right
+	}
+}
+
+fn step(mut c: (i32, i32), d: &Dir) -> (i32, i32) {
+	match d {
+		Dir::Left => c.0 -= 1,
+		Dir::Right => c.0 += 1,
+		Dir::Up => c.1 += 1,
+		Dir::Down => c.1 -= 1
+	}
+
+	c
+}
+
+fn sum_neighbors(hm: &HashMap<(i32, i32), i32>, c: &(i32, i32)) -> i32 {
+	let neighbors: Vec<(i32, i32)> = [
+		(c.0 + 1, c.1),
+		(c.0 + 1, c.1 + 1),
+		(c.0, c.1 + 1),
+		(c.0 - 1, c.1 + 1),
+		(c.0 - 1, c.1),
+		(c.0 - 1, c.1 - 1),
+		(c.0, c.1 - 1),
+		(c.0 + 1, c.1 - 1)
+	].into();
+
+	neighbors.iter().filter_map(|n| hm.get(n)).sum()
+}
 
 fn main() {
 	let val = 312051;
@@ -11,20 +54,42 @@ fn main() {
 		number = odd.pow(2);
 	}
 
-	println!("ring: {}, odd: {}, number: {}", ring, odd, number);
-
 	let mut coords: (i32, i32) = (ring, ring);
 	let side_length = ring * 2;
-	println!("coords: {:?}", coords);
-	println!("distance: {:?}", coords.0.abs() + coords.1.abs());
 
 	let diff = number - val;
-	println!("diff: {:?}", diff);
 
-	if !(diff < side_length) { panic!("diff ({}) not less than side length ({})", diff, side_length) };
+	if !(diff < side_length) {
+		panic!("diff ({}) not less than side length ({})", diff, side_length)
+	};
 
 	coords.1 -= diff;
-	println!("coords: {:?}", coords);
 	println!("distance: {:?}", coords.0.abs() + coords.1.abs());
+
+	let mut grid: HashMap<(i32, i32), i32> = HashMap::new();
+	let mut coords: (i32, i32) = (0, 0);
+	let mut num: i32 = 1;
+
+	grid.insert(coords, num);
+
+	let mut dir = Dir::Right;
+
+	while num < val {
+		// Move forward
+		coords = step(coords, &dir);
+
+		// Change directions if needed
+		if (!matches!(dir, Dir::Right) && (coords.0.abs() == coords.1.abs())) || (matches!(dir, Dir::Right) && coords.1 == -coords.0 + 1) {
+			dir = turn(&dir);
+		}
+
+		// Get neighbors values and sum
+		num = sum_neighbors(&grid, &coords);
+
+		// Insert to HashMap
+		grid.insert(coords, num);
+	}
+
+	println!("{:?}", num);
 }
 
