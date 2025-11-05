9cc6ec44ce155bf9084fd277f7f0adc3a6accb34
Brancen Gregory <brancengregory@gmail.com>
2025-11-05 01:05:20 -0800

    Complete 2017 day 4

--- 2017/04/rust/src/main.rs
+++ 2017/04/rust/src/main.rs
@@ -1,3 +1,59 @@
+use std::fs::read_to_string;
+
+fn read_input(p: &str) -> Result<Vec<Vec<String>>, Box<dyn std::error::Error>> {
+	let content = read_to_string(p)?;
+
+	Ok(
+		content
+			.lines()
+			.map(|x| {
+				x.split_whitespace().map(String::from).collect()
+			})
+			.collect()
+	)
+}
+
 fn main() {
-    println!("Hello, world!");
+	let input = read_input("../input").unwrap();
+
+	let res: usize = input
+		.clone()
+		.into_iter()
+		.filter(|p| {
+			let mut d = p.clone();
+
+			d.sort_unstable();
+			d.dedup();
+
+			d.len() == p.len()
+		})
+		.collect::<Vec<Vec<String>>>()
+		.len();
+
+  println!("{:?}", res);
+
+
+	let new_res: usize = input
+		.into_iter()
+		.filter(|p| {
+			let mut d = p.clone();
+
+			d = d.into_iter()
+				.map(|dd| {
+					let mut c: Vec<char> = dd.chars().collect();
+					c.sort_unstable();
+					c.iter().collect()
+				})
+				.collect();
+
+			d.sort_unstable();
+			d.dedup();
+
+			d.len() == p.len()
+		})
+		.collect::<Vec<Vec<String>>>()
+		.len();
+
+
+  println!("{:?}", new_res);
 }
