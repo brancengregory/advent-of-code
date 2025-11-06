6b21e430c91f35d72cb4744cc614f7998cf1cfcc
Brancen Gregory <brancengregory@gmail.com>
2025-11-05 23:58:30 -0600

    WIP

--- 2017/09/rust/src/main.rs
+++ 2017/09/rust/src/main.rs
@@ -1,3 +1,20 @@
+use std::fs::read_to_string;
+
+fn read_input(p: &str) -> String {
+	read_to_string(p).unwrap().trim().to_string()
+}
+
 fn main() {
-    println!("Hello, world!");
+	let input = read_input("../input");
+  println!("{:?}", input.chars());
+
+	let mut c: Vec<String> = input.chars().map(String::from).collect();
+
+	for (i, x) in c.clone().into_iter().enumerate() {
+		if x == "!" {
+			c.remove(i);
+			c.remove(i+1);
+		}
+		println!("{:?}: {:?}", i, x);
+	}
 }
