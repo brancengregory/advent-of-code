0b6fdd90cd8bc63bd4b1a4ed40412d9ad5c6a0dd
Brancen Gregory <brancengregory@gmail.com>
2025-11-05 16:39:33 -0800

    Attempt 2017 days 7 and 8 part 1

--- 2017/07/rust/src/main.rs
+++ 2017/07/rust/src/main.rs
@@ -1,3 +1,46 @@
+use std::fs::read_to_string;
+use std::collections::HashMap;
+
+fn read_input(p: &str) -> Vec<Vec<String>> {
+	let content = read_to_string(p).unwrap();
+
+	content
+		.lines()
+		.map(|x| {
+			x.split("->").map(String::from).collect()
+		})
+		.collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+	let input = read_input("../input");
+
+	let mut valmap: HashMap<String, u32> = HashMap::new();
+	let mut treemap: HashMap<String, Vec<String>> = HashMap::new();
+	input
+		.iter()
+		.for_each(|x| {
+			let mut splits = x[0].split_whitespace();
+			let k = splits.next().unwrap().to_string();
+			let v = splits.next().unwrap()
+				.strip_prefix("(").unwrap()
+				.strip_suffix(")").unwrap()
+				.parse::<u32>().unwrap();
+			valmap.insert(k.clone(), v);
+
+			if x.len() > 1 {
+				let children: Vec<String> = x[1]
+					.split(",")
+					.map(|x| x.trim().to_string())
+					.collect();
+
+				treemap.insert(k, children);
+			}
+		});
+
+	let all_nodes: Vec<&String> = valmap.keys().collect();
+	let all_children: Vec<&String> = treemap.values().flatten().collect();
+
+	let root_node: &String = all_nodes.into_iter().filter(|x| !all_children.contains(x)).next().unwrap();
+	println!("{:?}", root_node);
 }
--- 2017/08/rust/src/main.rs
+++ 2017/08/rust/src/main.rs
@@ -1,3 +1,59 @@
+use std::fs::read_to_string;
+use std::collections::HashMap;
+
+fn read_input(p: &str) -> Vec<Vec<String>> {
+	read_to_string(p)
+		.unwrap()
+		.lines()
+		.map(|x| {
+			x
+				.split(" if ")
+				.map(String::from)
+				.collect()
+		})
+		.collect()
+}
+
+
 fn main() {
-    println!("Hello, world!");
+	let input = read_input("../input");
+
+	let mut registers: HashMap<&str, i32> = HashMap::new();
+	input.iter().for_each(|x| {
+		registers.insert(x[0].split_whitespace().next().unwrap(), 0);
+		registers.insert(x[1].split_whitespace().next().unwrap(), 0);
+	});
+
+	input.iter().for_each(|x| {
+		let mut c_splits = x[1].split_whitespace();
+		let v1: &str = c_splits.next().unwrap();
+		let op: &str = c_splits.next().unwrap();
+		let v2: &str = c_splits.next().unwrap();
+
+		let res: bool = match op {
+			">" => v1 > v2,
+			"<" => v1 < v2,
+			">=" => v1 >= v2,
+			"<=" => v1 <= v2,
+			"==" => v1 == v2,
+			"!=" => v1 != v2,
+			_ => panic!()
+		};
+
+		if res {
+			let mut o_splits = x[0].split_whitespace();
+			let r: &str = o_splits.next().unwrap();
+			let d: &str = o_splits.next().unwrap();
+			let n: i32 = o_splits.next().unwrap().parse::<i32>().unwrap();
+
+			match d {
+				"inc" => registers.entry(r).and_modify(|x| *x += n),
+				"dec" => registers.entry(r).and_modify(|x| *x -= n),
+				_ => panic!()
+			};
+		}
+	});
+
+	let max_val = registers.values().max().unwrap();
+	println!("{:?}", max_val);
 }
