778eb2abdfaeb2812bc23b0258be086440e5a447
Brancen Gregory <brancengregory@gmail.com>
2025-11-08 16:09:42 -0600

    More idiomatic 2015-06

--- 2015/06/rust/src/main.rs
+++ 2015/06/rust/src/main.rs
@@ -1,100 +1,77 @@
-use std::{fs::File, io::Read};
+use std::fs::read_to_string;
 use std::fmt::Debug;
 use regex::Regex;
 
-use faer::Mat;
 
 #[derive(Clone)]
 struct Grid {
-    mat: Mat<f64>
+    lights: Vec<u32>,
+    width: usize
 }
 
 impl Grid {
-    fn turn_on(&mut self, start: Point, end: Point) {
-        let m = &mut self.mat;
-
-        for i in start.x..=end.x {
-            for j in start.y..=end.y {
-                m[(i as usize, j as usize)] = f64::from(1);
-            }
+    fn new(width: usize, height: usize) -> Self {
+        Self {
+            lights: vec![0; width * height],
+            width
         }
     }
 
-    fn nordic_turn_on(&mut self, start: Point, end: Point) {
-        let m = &mut self.mat;
-
-        for i in start.x..=end.x {
-            for j in start.y..=end.y {
-                let c = m[(i as usize, j as usize)];
-                m[(i as usize, j as usize)] = c + f64::from(1);
+    fn apply(&mut self, start: &Point, end: &Point, f: impl Fn(u32) -> u32) {
+        for y in start.y..=end.y {
+            for x in start.x..=end.x {
+                let index = (y as usize) * self.width + (x as usize);
+                self.lights[index] = f(self.lights[index]);
             }
         }
     }
-
-    fn toggle(&mut self, start: Point, end: Point) {
-        let m = &mut self.mat;
-        
-        for i in start.x..=end.x {
-            for j in start.y..=end.y {
-                m[(i as usize, j as usize)] = match m[(i as usize, j as usize)] {
-                    0.0 => f64::from(1),
-                    1.0 => f64::from(0),
-                    _ => panic!()
-                };
-            }
-        }
+    
+    fn turn_on(&mut self, start: &Point, end: &Point) {
+        self.apply(start, end, |_| 1);
     }
 
-    fn nordic_toggle(&mut self, start: Point, end: Point) {
-        let m = &mut self.mat;
-
-        for i in start.x..=end.x {
-            for j in start.y..=end.y {
-                let c = m[(i as usize, j as usize)];
-                m[(i as usize, j as usize)] = c + f64::from(2);
-            }
-        }
+    fn turn_off(&mut self, start: &Point, end: &Point) {
+        self.apply(start, end, |_| 0);
     }
 
-    fn turn_off(&mut self, start: Point, end: Point) {
-        let m = &mut self.mat;
-
-        for i in start.x..=end.x {
-            for j in start.y..=end.y {
-                m[(i as usize, j as usize)] = f64::from(0);
-            }
-        }
+    fn toggle(&mut self, start: &Point, end: &Point) {
+        self.apply(start, end, |v| 1 - v);
     }
 
-    fn nordic_turn_off(&mut self, start: Point, end: Point) {
-        let m = &mut self.mat;
-
-        for i in start.x..=end.x {
-            for j in start.y..=end.y {
-                let c = m[(i as usize, j as usize)];
-                if c - f64::from(1) < f64::from(0) {
-                    m[(i as usize, j as usize)] = f64::from(0);
-                } else {
-                    m[(i as usize, j as usize)] = c - f64::from(1);
-                }
-            }
-        }
+    fn nordic_turn_on(&mut self, start: &Point, end: &Point) {
+        self.apply(start, end, |v| v + 1);
     }
 
-    fn follow_instruction(&mut self, i: Instruction) {
+    fn nordic_turn_off(&mut self, start: &Point, end: &Point) {
+        self.apply(start, end, |v| v.saturating_sub(1));
+    }
+
+    fn nordic_toggle(&mut self, start: &Point, end: &Point) {
+        self.apply(start, end, |v| v + 2);
+    }
+
+    fn follow_instruction(&mut self, i: &Instruction) {
         match i.a {
-            Action::TurnOn => self.turn_on(i.s, i.e),
-            Action::Toggle => self.toggle(i.s, i.e),
-            Action::TurnOff => self.turn_off(i.s, i.e)
+            Action::TurnOn => self.turn_on(&i.s, &i.e),
+            Action::Toggle => self.toggle(&i.s, &i.e),
+            Action::TurnOff => self.turn_off(&i.s, &i.e)
         };
     }
 
-    fn follow_nordic_instruction(&mut self, i: Instruction) {
+    fn follow_nordic_instruction(&mut self, i: &Instruction) {
         match i.a {
-            Action::TurnOn => self.nordic_turn_on(i.s, i.e),
-            Action::Toggle => self.nordic_toggle(i.s, i.e),
-            Action::TurnOff => self.nordic_turn_off(i.s, i.e)
+            Action::TurnOn => self.nordic_turn_on(&i.s, &i.e),
+            Action::Toggle => self.nordic_toggle(&i.s, &i.e),
+            Action::TurnOff => self.nordic_turn_off(&i.s, &i.e)
         };
+    }
+
+    fn sum(&self) -> u32 {
+        self.lights.iter().sum()
+    }
+
+    fn reset(&mut self) {
+        self.lights.fill(0);
     }
 }
 
@@ -143,81 +120,39 @@ struct Instruction {
     e: Point
 }
 
-impl From<&str> for Instruction {
-    fn from(value: &str) -> Self {
-        todo!()
-    }
-}
-
-fn parse_command(input: &str) -> Vec<String> {
-    let re = Regex::new(r"(.+?) (\d+,\d+) through (\d+,\d+)").unwrap();
-    let captures = re.captures(input).unwrap();
-
-    vec![
-        captures[1].to_string(), // The command, e.g., "toggle"
-        captures[2].to_string(), // The first coordinate pair, e.g., "753,664"
-        captures[3].to_string(), // The second coordinate pair, e.g., "970,926"
-    ]
-}
-
-
 fn read_input(p: &str) -> Vec<Instruction> {
-    let mut f = File::open(p).unwrap();
-    let mut s: String = String::new();
+    read_to_string(p) 
+        .unwrap()
+        .lines()
+        .map(|x| {
+            let re = Regex::new(r"(.+?) (\d+,\d+) through (\d+,\d+)").unwrap();
+            let captures = re.captures(x).unwrap();
 
-    f.read_to_string(&mut s).unwrap();
-
-    let i: Vec<Vec<String>> = s
-        .trim()
-        .split('\n')
-        .map(|x| parse_command(x.trim()))
-        .collect();
-
-    let mut instructions: Vec<Instruction> = Vec::new();
-
-    for x in i {
-        instructions.push(
             Instruction {
-                a: Action::from(x[0].as_str()),
-                s: Point::from(x[1].as_str()),
-                e: Point::from(x[2].as_str())
+                a: Action::from(&captures[1]),
+                s: Point::from(&captures[2]),
+                e: Point::from(&captures[3])
             }
-        )
-    }
-
-    return instructions
+        })
+        .collect()
 }
 
 fn main() {
     let input = read_input("../input");
 
-    let mut m: Grid = Grid {
-        mat: Mat::zeros(1000, 1000)
-    };
+    let mut m: Grid = Grid::new(1000, 1000);
 
-    for x in input.clone() {
-        m.follow_instruction(x)
+    for i in input.iter() {
+        m.follow_instruction(i);
     }
 
-    let ans1 = m.mat
-        .col_iter()
-        .map(|x| x.sum())
-        .reduce(|acc, e| acc + e)
-        .unwrap();
+    println!("Problem 1: {}", m.sum());
 
-    println!("Problem 1: {:?}", ans1 as i64);
+    m.reset();
 
-    m.mat = Mat::zeros(1000, 1000);
-
-    for x in input {
-        m.follow_nordic_instruction(x)
+    for i in input.iter() {
+        m.follow_nordic_instruction(i);
     }
 
-    let ans2 = m.mat
-        .col_iter()
-        .map(|x| x.sum())
-        .reduce(|acc, e| acc + e)
-        .unwrap();
-
-    println!("Problem 2: {:?}", ans2 as i64);
+    println!("Problem 2: {}", m.sum());
 }
