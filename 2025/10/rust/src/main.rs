cdc026b7dd252f365a0a1bbc6dc57149f50e10cb
Brancen Gregory <brancengregory@gmail.com>
2025-12-09 23:59:45 -0600

    Parse 2025 day 10

--- 2025/10/rust/src/main.rs
+++ 2025/10/rust/src/main.rs
@@ -1,3 +1,55 @@
+#[derive(Debug)]
+struct Machine {
+    lights: Vec<u8>,
+    buttons: Vec<Vec<usize>>,
+    joltages: Vec<usize>
+}
+
+fn read_input(p: &str) -> Vec<Machine> {
+    std::fs::read_to_string(p).expect("Input file {} not found")
+        .lines()
+        .map(|x| {
+            let mut splits = x.split_whitespace();
+
+            let lights_str = splits.next().unwrap();
+
+            let lights: Vec<u8> = lights_str
+                .chars()
+                .filter_map(|c| match c {
+                    '.' => Some(0),
+                    '#' => Some(1),
+                    _ => None,
+                })
+                .collect();
+
+            let joltages_str = splits.next_back().unwrap();
+
+            let joltages: Vec<usize> = joltages_str
+                .trim_matches(|c| c == '{' || c == '}')
+                .split(",")
+                .map(|s| {
+                    s.parse::<usize>().unwrap()
+                })
+                .collect();
+            
+            let buttons = splits
+                .map(|s| {
+                    s.chars()
+                        .filter_map(|c| c.to_digit(10).map(|d| d as usize))
+                        .collect()
+                })
+                .collect();
+            
+            Machine {
+                lights,
+                buttons,
+                joltages,
+            }
+        })
+        .collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_input("../input");
+    println!("{:#?}", input);
 }
